a2d3b00f5190d05fd2e679a5e3863a2ea96cd430
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 <4727yesuraju@gmail.com>
2024-02-24 14:24:59 +0530

    al

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { Card } from './components/card/Card';
+import image1 from './images/image1.jpeg'
+import image2 from './images/image2.jpeg'
+import image3 from './images/image3.jpeg'
+import image4 from './images/image4.jpeg'
+import image5 from './images/image5.jpeg'
+import image6 from './images/image6.jpeg'
+
+import React,{useState} from 'react'
+
 
 function App() {
+  const cards = [
+    {
+      url:image1,
+      title:"Audi R8",
+      description:"The definition of a supercar describes an expensive car that's flashy,"
+    },
+    {
+      url:image2,
+      title:"Maserati MC20",
+      description:"The definition of a supercar describes an expensive car that's flashy"
+    },
+    {
+      url:image3,
+      title:"Porsche 911 GT3 RS",
+      description:"The definition of a supercar describes an expensive car that's flashy"
+    },
+    {
+      url:image4,
+      title:"Lamborghini Huracan Tecnica",
+      description:"The definition of a supercar describes an expensive car that's flashy"
+    },
+    {
+      url:image5,
+      title:"Rimac Nevera",
+      description:"The definition of a supercar describes an expensive car that's flashy"
+    },
+    {
+      url:image6,
+      title:"car2",
+      description:"The definition of a supercar describes an expensive car that's flashy"
+    },
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="app">
+       {
+         cards.map((card,index)=><Card key={index} card={card}/>)
+       }
+     </div>
   );
 }
 
